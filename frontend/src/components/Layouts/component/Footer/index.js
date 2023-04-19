dfd630e3334686804147b34ebdc4cf134f5ab08e
TDungx <21020753@vnu.edu.vn>
2023-04-19 11:52:28 +0700

    Add footer component

--- frontend/src/components/Layouts/component/Footer/index.js
+++ frontend/src/components/Layouts/component/Footer/index.js
@@ -1,5 +1,54 @@
+import style from './Footer.module.scss'
+
 function Footer() {
-    return <h2>Footer Component</h2>;
+    return (
+        <>
+            <div className={style.footerContainer}>
+                <div className={style.contentContainer}>
+                    <ul className={style.servicesColumn}>
+                        Services
+                        <br />
+                        <br />
+                        <li>Online 24/7</li>
+                        <li>Buy quickly</li>
+                        <li>Any Questions?</li>
+                    </ul>
+                    <ul className={style.shopColumn}>
+                        Shop
+                        <br />
+                        <br />
+                        <li>Foods</li>
+                        <li>Drinks</li>
+                        <li>Shirts</li>
+                    </ul>
+                    <ul className={style.membersColumn}>
+                        Members
+                        <br />
+                        <br />
+                        <li>Trần Anh Dũng</li>
+                        <li>Nguyễn Cao Đức</li>
+                        <li>Trương Quang Đạt</li>
+                        <li>Đặng Tiến Dũng</li>
+                    </ul>
+                </div>
+                <div className={style.contactContainer}>
+                    <a href="#" className={style.logo}>Logo</a>
+                    <div className={style.listContact}>
+                        <a href="#" className={style.instagram}>
+                            <img src="" alt="" />
+                            in
+                        </a>
+                        <a href="#" className={style.facebook}>
+                            fb
+                        </a>
+                        <a href="#" className={style.twitter}>
+                            tw
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Footer;
